952337007b903cf2463a5bcce1cd6507d28b4c67
Steven <steven@stevengouws.com>
2021-03-19 12:01:51 +0000

    Read and display Weather temperature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import systemInformation from 'systeminformation';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import propTypes from 'prop-types';
 import './App.global.css';
 
 async function getCpuTemperature() {
@@ -11,16 +12,58 @@ async function getCpuTemperature() {
   return cpuTemperatureDetails.cores[0];
 }
 
+async function getOutsideTemperature() {
+  const response: any = await fetch(
+    'https://api.openweathermap.org/data/2.5/weather?lat=51.504560&lon=-0.212970&units=metric&appid=9e1968833a7cced162c050f17816cbdc'
+  );
+  if (!response.ok) throw response;
+  const json = await response.json();
+  return json.main.temp;
+}
+
+export function Main({ cpuTemperature, outsideTemperature }: any) {
+  return (
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th>CPU temperature</th>
+            <th>Outside temperature</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{cpuTemperature}</td>
+            <td>{outsideTemperature}</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
+}
+
+Main.propTypes = {
+  cpuTemperature: propTypes.number.isRequired,
+  outsideTemperature: propTypes.number.isRequired,
+};
+
 const Content = () => {
   const [cpuTemperature, setCpuTemperature] = useState(0);
+  const [outsideTemperature, setOutsideTemperature] = useState(0);
 
   useEffect(() => {
-    setInterval(async () => {
-      setCpuTemperature(await getCpuTemperature());
-    }, 1000);
+    setInterval(() => {
+      getCpuTemperature().then(setCpuTemperature).catch(console.error);
+      getOutsideTemperature().then(setOutsideTemperature).catch(console.error);
+    }, 10000);
   }, []);
 
-  return <div>{cpuTemperature}</div>;
+  return (
+    <Main
+      cpuTemperature={cpuTemperature}
+      outsideTemperature={outsideTemperature}
+    />
+  );
 };
 
 export default function App() {
